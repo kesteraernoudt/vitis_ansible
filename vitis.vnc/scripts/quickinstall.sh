1954bd3bf4614c41b53ca90204502a3d9726748e
Kester Aernoudt <kesteraernoudt@yahoo.com>
2020-03-31 10:29:33 +0200

    First commit

---
+++ vitis.vnc/scripts/quickinstall.sh
@@ -0,0 +1,49 @@
+#!/bin/bash
+# 
+# Install VNC locally, with default settings.
+#
+# Instructions:
+#
+# curl -sL https://raw.githubusercontent.com/sdarwin/Ansible-VNC/master/scripts/quickinstall.sh | sudo bash
+#
+#
+
+set -x
+set -e
+PATH=/usr/local/bin:$PATH
+
+if [[ -f /usr/bin/apt-get ]]
+then
+    sudo apt-get update
+fi
+
+if ! which git
+then
+  if [[ -f /usr/bin/apt-get ]]
+  then
+    sudo apt-get install -y git
+  fi
+  if [[ -f /bin/yum ]]
+  then
+    sudo yum install -y git
+  fi
+fi
+
+if ! which ansible 
+then
+  if [[ -f /usr/bin/apt-get ]]
+  then
+    sudo apt-get install -y python3-pip
+    sudo pip3 install ansible
+  fi
+  if [[ -f /bin/yum ]]
+  then
+    . /etc/os-release
+    sudo yum install -y https://dl.fedoraproject.org/pub/epel/epel-release-latest-${VERSION_ID:0:1}.noarch.rpm
+    sudo yum install -y python3-pip
+    sudo pip3 install ansible
+  fi
+fi
+
+sudo mkdir -p /etc/ansible/roles
+sudo env PATH=$PATH ansible-pull -U https://github.com/sdarwin/Ansible-VNC -d /etc/ansible/roles/sdarwin.VNC -i scripts/localinventory default.yml
